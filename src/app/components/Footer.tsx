a305bdc80c5e9af0a7c047326e4057f9432f7d05
Maitann <hiagomaitan@gmail.com>
2023-09-13 04:18:47 -0300

    FEAT: footer gap

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="w-full h-[26px] fixed bottom-0 items-center bg-blue-900 flex justify-end text-white pr-32">
+    <div className="w-full h-[26px] fixed bottom-0 items-center bg-blue-900 flex gap-1 justify-end text-white pr-32">
       <p>Hero Tickets ® {new Date().getFullYear()} |</p>
       <p>Todos os Direitos Reservados</p>
     </div>
